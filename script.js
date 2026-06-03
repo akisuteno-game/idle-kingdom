39785ab828a07bc2438849060d71d26792a1b916
akisuteno-game <akisuteno5160@gmail.com>
2026-06-03 19:15:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,39 +5,40 @@ function updateGold() {
 }
 
 function work() {
-    gold += 1;
+    gold++;
     updateGold();
 }
 
-document.addEventListener(
-    "gesturestart",
-    function (e) {
-        e.preventDefault();
-    },
-    { passive: false }
-);
+function stopEvent(e) {
+    e.preventDefault();
+}
 
-document.addEventListener(
-    "gesturechange",
-    function (e) {
-        e.preventDefault();
-    },
-    { passive: false }
-);
+document.addEventListener("gesturestart", stopEvent, {
+    passive: false
+});
 
-document.addEventListener(
-    "gestureend",
-    function (e) {
-        e.preventDefault();
-    },
-    { passive: false }
-);
+document.addEventListener("gesturechange", stopEvent, {
+    passive: false
+});
+
+document.addEventListener("gestureend", stopEvent, {
+    passive: false
+});
+
+document.addEventListener("touchmove", stopEvent, {
+    passive: false
+});
+
+document.addEventListener("dblclick", stopEvent, {
+    passive: false
+});
 
 let lastTouchEnd = 0;
 
 document.addEventListener(
     "touchend",
     function (event) {
+
         const now = Date.now();
 
         if (now - lastTouchEnd <= 300) {
