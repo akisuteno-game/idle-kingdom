845b332b4f20a0a48c285ec8ca804c9b8c0ec35f
akisuteno-game <akisuteno5160@gmail.com>
2026-06-03 19:11:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,43 @@ function work() {
     gold += 1;
     updateGold();
 }
+
+document.addEventListener(
+    "gesturestart",
+    function (e) {
+        e.preventDefault();
+    },
+    { passive: false }
+);
+
+document.addEventListener(
+    "gesturechange",
+    function (e) {
+        e.preventDefault();
+    },
+    { passive: false }
+);
+
+document.addEventListener(
+    "gestureend",
+    function (e) {
+        e.preventDefault();
+    },
+    { passive: false }
+);
+
+let lastTouchEnd = 0;
+
+document.addEventListener(
+    "touchend",
+    function (event) {
+        const now = Date.now();
+
+        if (now - lastTouchEnd <= 300) {
+            event.preventDefault();
+        }
+
+        lastTouchEnd = now;
+    },
+    false
+);
