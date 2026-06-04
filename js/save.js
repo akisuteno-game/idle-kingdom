7fba97b16f3b5fa5f9dcad0c2e9831f6b974b38c
akisuteno-game <akisuteno5160@gmail.com>
2026-06-04 20:39:25 +0900

    Update save.js

--- js/save.js
+++ js/save.js
@@ -1,12 +1,18 @@
 function saveGame() {
-
     localStorage.setItem(
         "idleKingdomSave",
         JSON.stringify(game)
     );
-
 }
 
 function loadGame() {
 
+    const save =
+        localStorage.getItem("idleKingdomSave");
+
+    if (!save) return;
+
+    const data = JSON.parse(save);
+
+    Object.assign(game, data);
 }
