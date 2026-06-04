b28beea216b6ce10b6731a0cd2a4f8384fa3926f
akisuteno-game <akisuteno5160@gmail.com>
2026-06-04 20:39:56 +0900

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,11 +1,21 @@
-function startJob() {
-
-}
-
-function completeJob() {
-
-}
-
 function gameLoop() {
 
+    game.age += 0.001;
+
+    if (game.currentJob) {
+
+        game.progress +=
+            100 /
+            jobs[game.currentJob].duration;
+
+        if (game.progress >= 100) {
+
+            game.progress = 0;
+
+            game.gold +=
+                jobs[game.currentJob].reward;
+        }
+    }
+
+    updateUI();
 }
