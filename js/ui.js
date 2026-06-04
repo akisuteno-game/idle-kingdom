961a5de3855d2b66d8eb0f49b431e0512211554e
akisuteno-game <akisuteno5160@gmail.com>
2026-06-04 20:39:41 +0900

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,15 +1,16 @@
-function updateGold() {
-
-}
-
-function updateAge() {
-
-}
-
-function updateJob() {
-
-}
-
 function updateUI() {
 
+    document.getElementById("gold").textContent =
+        game.gold;
+
+    document.getElementById("age").textContent =
+        game.age.toFixed(1);
+
+    document.getElementById("job").textContent =
+        game.currentJob
+            ? jobs[game.currentJob].name
+            : "なし";
+
+    document.getElementById("progressFill").style.width =
+        game.progress + "%";
 }
